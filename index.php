1004404786bc068f15b799c6e5e6122020f40aaa
Fábio Santos <fabiosant2005@hotmail.com>
2023-12-02 07:32:19 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"><![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"><![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"><![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="pt-br"><!--<![endif]-->
+<head>
+<!-- Website Template designed by www.downloadwebsitetemplates.co.uk -->
+<meta charset="UTF-8">
+<title>Câmara de Afonso Cunha - Site em Manutenção</title>
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72.png">
+<link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57.png">
+<link rel="shortcut icon" href="images/ico/favicon.png">
+<!--[if IE]><![endif]-->
+<link rel="stylesheet" href="css/style.css">
+<script src="js/jquery.js"></script>
+<script src="js/countdown.js"></script>
+<script src="js/owlcarousel.js"></script>
+<script src="js/uikit.scrollspy.js"></script>
+<script src="js/scripts.js"></script>
+<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body id="backtotop">
+
+<div class="fullwidth clearfix">
+	<div id="topcontainer" class="bodycontainer clearfix" data-uk-scrollspy="{cls:'uk-animation-fade', delay: 300, repeat: true}">
+		
+		<p><span class="fa fa-gavel"></span></p>
+		<h1><span>Manutenção</span><br />Programada</h1>
+		<p>Estamos trabalhando para melhor disponibilizar os Serviços Online da Câmara de Afonso Cunha</p>
+		
+	</div>
+</div>
+
+
+<div class="fullwidth clearfix">
+	<div id="footercont" class="bodycontainer clearfix" data-uk-scrollspy="{cls:'uk-animation-fade', delay: 300, repeat: true}">
+
+		<div id="socialmedia" class="clearfix">
+			<ul>
+				<li><a title="" href="#" target="_blank" rel="external"><span class="fa fa-facebook"></span></a></li>
+			
+			</ul>
+		</div>
+		<h3><span>Câmara de Afonso Cunha</h3>
+		<p>(98) 3477-1892</p>
+	</div>
+</div>
+   
+</body>
+</html>
